327233171e70b53594dd0e181b9a83ab0029afbd
carlossalnav <carlos.salnav@gmail.com>
2021-07-23 18:46:23 -0500

    [added] show and send messages

--- src/comps/Chat.js
+++ src/comps/Chat.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import SignOut from './SignOut'
+import {useState, useEffect} from 'react';
+import { db } from '../firebase';
+import SendMessage from './SendMessage';
 
 function Chat() {
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
     return (
         <div>
             <SignOut/>
-            CHAT
+            {messages.map(({id, text, photoURL}) =>(
+                <div key = {id}>
+                    <img src = {photoURL} alt = "profile photo"/>
+                    <p>{text}</p>
+                </div>
+            ))}
+            <SendMessage/>
         </div>
     )
 }
